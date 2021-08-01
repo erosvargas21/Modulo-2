7f585d7b0f35bb11d1e20a94c945b11fbcffd402
erosvargas21 <evargast@uni.pe>
2021-08-01 06:02:36 -0500

    Modulo 2

---
+++ Modulo-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="minitorch",
+    version="0.1",
+    packages=[
+        "minitorch"
+    ],
+    package_data={"minitorch": []},
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
+ 
